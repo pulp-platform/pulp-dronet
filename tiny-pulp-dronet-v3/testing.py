f5cd988560bac937db905978b4be61b5608aa92e
Lorenzo Lamberti <lorenzo.lamberti@unibo.it>
2024-08-13 16:04:41 +0200

    fix init of weights

--- tiny-pulp-dronet-v3/testing.py
+++ tiny-pulp-dronet-v3/testing.py
@@ -285,7 +285,7 @@ def main():
     elif args.block_type == "IRLB":
         net = dronet(depth_mult=args.depth_mult, block_class=Inverted_Linear_Bottleneck, bypass=args.bypass)
 
-    net = load_weights_into_network(args.model_weights_path, net, args.resume_training, device)
+    net = load_weights_into_network(args.model_weights_path, net, device)
 
     net.to(device)
     summary(net, input_size=(1, 1, 200, 200))
